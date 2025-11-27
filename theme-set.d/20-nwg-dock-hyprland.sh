6edcbbb6804e6f476597d20280b48e9bb324d8f8
imbypass <coleschaefer1@gmail.com>
2025-11-27 08:42:45 -0500

    added experimental nwg-dock support

---
+++ theme-set.d/20-nwg-dock-hyprland.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+output_file="$HOME/.config/omarchy/current/theme/nwg-dock.css"
+
+if [[ ! -f "$output_file" ]]; then
+    cat > "$output_file" << EOF
+window {
+    background: #${primary_background}; /* base01 */
+    border-color: #${bright_black}; /* base02 */
+    border: 2px solid #${bright_black};
+}
+
+button,
+image {
+    color: #${bright_white}; /* base07 */
+}
+
+button {
+    color: #${normal_white}; /* base05 */
+    padding: 3px;
+}
+
+button:hover {
+    background-color: rgba($rgb_bright_black, 0.5); /* base02 */
+}
+EOF
+fi
+
+style_file="$HOME/.config/nwg-dock-hyprland/style.css"
+if [[ ! -f $style_file ]]; then
+    cat > $style_file << EOF
+@import url("./colors.css");
+
+window {
+    border-radius: 12px;
+    border-style: solid;
+    border-width: 2px;
+}
+
+#box {
+    padding: 6px;
+}
+
+button,
+image {
+    background: none;
+    border-style: none;
+    box-shadow: none;
+}
+
+button {
+    padding: 5px;
+    margin-left: 4px;
+    margin-right: 4px;
+    margin-top: 4px;
+    font-size: 18px;
+}
+
+button:focus {
+    box-shadow: none;
+}
+EOF
+fi
+
+cp "$output_file" "$HOME/.config/nwg-dock-hyprland/colors.css"
+killall nwg-dock-hyprland
+nwg-dock-hyprland -r -mb 10 -mt 10 -i 22 -x -nolauncher & disown
+
+success "Dock theme updated!"
